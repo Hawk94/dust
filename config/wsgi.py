f4b26554161feb745a3a0d7b4ec175a64f11c5e6
Hawk94 <x99tom.miller@gmail.com>
2017-07-20 15:06:49 +0200

    Making it work...

--- config/wsgi.py
+++ config/wsgi.py
@@ -21,7 +21,7 @@ from django.core.wsgi import get_wsgi_application
 # This allows easy placement of apps within the interior
 # dust directory.
 app_path = os.path.dirname(os.path.abspath(__file__)).replace('/config', '')
-sys.path.append(os.path.join(app_path, 'dust'))
+sys.path.append(os.path.join(app_path, 'backend'))
 
 if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
     from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
